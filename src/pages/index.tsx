e8eceb5cee66aca06f1c8d89d365091493b728d3
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-11-29 13:59:59 -0300

    Validação dos dados com yup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,25 @@ import React from 'next';
 import { Flex, Button, Stack } from '@chakra-ui/react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Input } from '../components/Forms/Input';
-import { resolve } from 'path';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type SignInFormData = {
   email: string;
   password: string;
 }
 
+const signInFormSchema = yup.object({
+  email: yup.string().required("E-mail obrigatório").email('E-mail inválido'),
+  password: yup.string().required("Senha obrigatória"),
+})
+
 export default function SignIn() {
 
-  const { register, handleSubmit, formState } = useForm();
-
+  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+  
   const handleSignIn: SubmitHandler<SignInFormData> = async(values) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
 
@@ -38,12 +46,25 @@ export default function SignIn() {
       >
         <Stack spacing='4'>
 
-          <Input name='email' type="email" id='email' label='E-mail' {...register('email')} />
-          <Input name='password' type="password" id='password' label='Senha' {...register('password')} />
+          <Input
+          name="email"
+          type="email"
+          label="E-mail"
+          error={errors.email}
+          {...register("email")}
+          />
+
+          <Input 
+          name='password' 
+          type="password" 
+          label='Senha'
+          error={errors.password}
+          {...register('password')} 
+          />
 
         </Stack>
 
-        <Button type='submit' mt='6' colorScheme="pink" isLoading={formState.isSubmitting}> Entrar </Button>
+        <Button type='submit' mt='6' colorScheme="pink" isLoading={isSubmitting}> Entrar </Button>
 
       </Flex>
     </Flex>
