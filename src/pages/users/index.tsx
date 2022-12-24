9b62a423fe6582ea7998a181a6307c0a201c2034
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-12-23 22:25:35 -0300

    Configurando data e mostrando dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,21 @@
-import { Box, Button, Checkbox, Flex, Heading, HStack, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiDeleteBinLine, RiEditLine } from "react-icons/ri";
@@ -6,97 +23,139 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
+import { replaceAt } from "react-query/types/core/utils";
 
 export default function UserList() {
-    const { data, isLoading, error } = useQuery(['users'], async () => {
-        const response = await fetch("http://localhost:3000/api/users")
-        const data = await response.json();
+  const { data, isLoading, error } = useQuery(["users"], async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
 
-        return data;
+    const users = data.users.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: "long",
+                year: 'numeric',
+            })
+        }
     })
 
-    const isWidhtVersion = useBreakpointValue({
-        base: false,
-        lg: true
-    })
+    return users;
+  });
 
-    useEffect(() => {
-        fetch("http://localhost:3000/api/users")
-            .then(response => response.json())
-            .then(data => console.log(data))
-    }, [])
+  const isWidhtVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
 
-    return (
-        <Box>
-            <Header />
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
 
-            <Flex w="100%" my="6" maxWidth="1480" mx="auto" px="6">
-                <SideBar />
+  return (
+    <Box>
+      <Header />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+      <Flex w="100%" my="6" maxWidth="1480" mx="auto" px="6">
+        <SideBar />
 
-                        <Link href="/users/create" passHref>
-                            <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
-                                Criar Novo
-                            </Button>
-                        </Link>
-                    </Flex>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
-                    {isLoading ? (
-                        <Flex justify="center">
-                            <Spinner />
-                        </Flex>
-                    ) : error ? (
-                        <Flex justify="center">
-                            <Text> Falha ao obter dados dos usuários </Text>
-                        </Flex>
-                    ) : (
-                        <>
-                            <Table colorScheme="whiteAlpha">
-                                <Thead>
-                                    <Tr>
-                                        <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                                            <Checkbox colorScheme="pink" />
-                                        </Th>
-                                        <Th> Usuário </Th>
-                                        <Th> Data de cadastro </Th>
-                                        <Th w="8"> </Th>
-                                        {/* <Th w="8"> </Th> */}
-                                    </Tr>
-                                </Thead>
-                                <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Luís Henrique</Text>
-                                                <Text fontSize="sm" color="gray.300">luissh.dev@gmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWidhtVersion && <Td> 21 de outubro, 2022</Td>}
-                                        <Td>
-                                            <HStack spacing="2">
-                                                <Button as="a" size="sm" fontSize="sm" colorScheme="green" leftIcon={<Icon as={RiEditLine} fontSize="16" />}>
-                                                    {isWidhtVersion ? 'Editar' : ''}
-                                                </Button>
-                                                <Button as="a" size="sm" fontSize="sm" colorScheme="red" leftIcon={<Icon as={RiDeleteBinLine} fontSize="16  " />}>
-                                                    {isWidhtVersion ? 'Apagar' : ''}
-                                                </Button>
-                                            </HStack>
-                                        </Td>
-                                    </Tr>
-                                </Tbody>
-                            </Table>
-                            <Pagination />
-                        </>
-                    )}
-                </Box>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar Novo
+              </Button>
+            </Link>
+          </Flex>
+
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
             </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text> Falha ao obter dados dos usuários </Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th> Usuário </Th>
+                    <Th> Data de cadastro </Th>
+                    <Th w="8"> </Th>
+                    {/* <Th w="8"> </Th> */}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  { data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWidhtVersion && <Td> {user.createdAt}</Td>}
+                        <Td>
+                          <HStack spacing="2">
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="green"
+                              leftIcon={<Icon as={RiEditLine} fontSize="16" />}
+                            >
+                              {isWidhtVersion ? "Editar" : ""}
+                            </Button>
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="red"
+                              leftIcon={
+                                <Icon as={RiDeleteBinLine} fontSize="16  " />
+                              }
+                            >
+                              {isWidhtVersion ? "Apagar" : ""}
+                            </Button>
+                          </HStack>
+                        </Td>
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
-    );
+      </Flex>
+    </Box>
+  );
 }
